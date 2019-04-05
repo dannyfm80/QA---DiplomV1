533b82302d620927235045466e582b87e56df755
Danny Flores <dannyfm80@gmail.com>
2019-04-04 23:39:07 -0400

    first login

---
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
@@ -0,0 +1,25 @@
+package org.umssdiplo.automationv01.stepdefinitionproject;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import org.umssdiplo.automationv01.core.managepage.Login.SSIService;
+import org.umssdiplo.automationv01.core.utils.LoadPage;
+
+public class StepsDefinitionPHPtravel {
+    private SSIService ssiService;
+
+    @Given("^'SisSecurity' page is loaded$")
+    public void phpTravelPageIsLoaded() throws Throwable {
+        ssiService = LoadPage.loginPage();
+    }
+
+    @And("^set my credentials on 'Login' page$")
+    public void setMyCredentialsOnLoginPage() throws Throwable {
+        ssiService.setCredentials();
+    }
+
+//    @And("^click 'Registrar Maquinaria' tab in 'Header' page$")
+//    public void clickRegistrarMaquinariaTabInHeaderPage() throws Throwable {
+//        ssiService.clickRegistrarMaquinariTab();
+//    }
+}
